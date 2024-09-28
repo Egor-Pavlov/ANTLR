9824d370e6f55e93b683e9edc257b5253a47b482
Егор Павлов <egor12200@gmail.com>
2024-09-28 14:05:58 +0700

    очистил метод

--- CompilersLab1/Form1.cs
+++ CompilersLab1/Form1.cs
@@ -610,35 +610,7 @@ namespace CompilersLab1
 
         private void AntlrBTN_Click(object sender, EventArgs e)
         {
-            // Получаем текст из поля ввода
-            string input = txtInput.Text;
-
-            try
-            {
-                // Создаем поток символов для ANTLR
-                AntlrInputStream inputStream = new AntlrInputStream(input);
-
-                // Создаем лексер
-                IdentifierGrammarLexer lexer = new IdentifierGrammarLexer(inputStream);
-
-                // Создаем токены на основе лексера
-                CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-
-                // Создаем парсер
-                IdentifierGrammarParser parser = new IdentifierGrammarParser(tokenStream);
-
-                // Парсим текст с помощью стартового правила грамматики
-
-                MyGrammarParser.StartRuleContext context = parser.startRule();
-
-                // Обработка успешного парсинга (например, вывод структуры дерева)
-                lblResult.Text = "Парсинг успешен: " + context.ToStringTree(parser);
-            }
-            catch (Exception ex)
-            {
-                // Вывод ошибок в случае неудачи
-                lblResult.Text = "Ошибка: " + ex.Message;
-            }
+            
         }
     }
 }
